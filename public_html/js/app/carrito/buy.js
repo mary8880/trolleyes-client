229ffb409a7009d16a48904e39844a7d622b2154
Maria <Maria@192.168.1.6>
2019-01-23 13:46:16 +0100

    empty carrito buy

--- public_html/js/app/carrito/buy.js
+++ public_html/js/app/carrito/buy.js
@@ -3,20 +3,24 @@
 moduleProducto.controller('productoBuyController', ['$scope', '$http', '$location', 'toolService', '$routeParams', 'sessionService',
     function ($scope, $http, $location, toolService, $routeParams, oSessionService) {
         $scope.ruta = $location.path();
+        $scope.comprado = false;
 
-
-            $scope.buy= function () {
+        $scope.buy = function () {
             $http({
                 method: 'GET',
                 url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=buy'
             }).then(function (response) {
                 $scope.status = response.data.status;
                 $scope.ajaxData = response.data.message;
+                $scope.comprado = true;
+
 //                countCarritoService.updateCarrito();
             }, function (response) {
                 $scope.status = response.data.status;
                 $scope.ajaxData = response.data.message || 'Request failed';
+                $scope.comprado = false;
             });
+
         }
 //            $http({
 //                method: 'GET',
@@ -29,6 +33,23 @@ moduleProducto.controller('productoBuyController', ['$scope', '$http', '$locatio
 //                $scope.status = response.status;
 //            });
 //        }
+        $scope.empty = function () {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=empty'
+            }).then(function (response) {
+                $scope.status = response.data.status;
+                $scope.ajaxDataProductos = response.data.message;
+                $scope.carritoVacio = true;
+//                countCarritoService.updateCarrito();
+            }, function (response) {
+                $scope.status = response.data.status;
+                $scope.ajaxDataProductos = response.data.message || 'Request failed';
+                $scope.carritoVacio = false;
+            });
+
+        }
+
 
         $scope.isActive = toolService.isActive;
 
--- public_html/js/app/carrito/show.js
+++ public_html/js/app/carrito/show.js
@@ -22,6 +22,10 @@ moduleProducto.controller('productoShowController', ['$scope', '$http', '$locati
         }).then(function (response) {
             $scope.status = response.status;
             $scope.ajaxDataProductos = response.data.message
+            var data=$scope.ajaxDataProductos ;
+            if(data===null){
+                $scope.carritoVacio=true;
+            }
         }, function (response) {
             $scope.ajaxDataProductos = response.data.message || 'Request failed';
             $scope.status = response.status;
@@ -65,10 +69,12 @@ moduleProducto.controller('productoShowController', ['$scope', '$http', '$locati
             }).then(function (response) {
                 $scope.status = response.data.status;
                 $scope.ajaxDataProductos = response.data.message;
+                $scope.carritoVacio=true;
 //                countCarritoService.updateCarrito();
             }, function (response) {
                 $scope.status = response.data.status;
                 $scope.ajaxDataProductos = response.data.message || 'Request failed';
+                $scope.carritoVacio=false;
             });
 
         }
@@ -87,29 +93,6 @@ moduleProducto.controller('productoShowController', ['$scope', '$http', '$locati
             });
 
         }
-//        $scope.buy= function () {
-//            $http({
-//                method: 'GET',
-//                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=buy'
-//            }).then(function (response) {
-//                $scope.status = response.data.status;
-//                $scope.ajaxData = response.data.message;
-////                countCarritoService.updateCarrito();
-//            }, function (response) {
-//                $scope.status = response.data.status;
-//                $scope.ajaxData = response.data.message || 'Request failed';
-//            });
-//            $http({
-//                method: 'GET',
-//                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=show'
-//            }).then(function (response) {
-//                $scope.status = response.status;
-//                $scope.ajaxDataProductos = response.data.message
-//            }, function (response) {
-//                $scope.ajaxDataProductos = response.data.message || 'Request failed';
-//                $scope.status = response.status;
-//            });
-//        }
 
         $scope.isActive = toolService.isActive;
 
